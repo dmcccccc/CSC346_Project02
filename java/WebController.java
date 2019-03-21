99df99fcd85a7034f401251a8bda9660a63bd38d
Mingcheng Dai <dmcccccc@gmail.com>
2019-03-21 12:56:29 -0700

    Create WebController.java

---
+++ java/WebController.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class WebController {
+    /*TODO: Add jdbc URL, username and passwprd */
+    public static final String url = "";
+    public static final String username = "";
+    public static final String password = "";
+
+    //TODO: Connect to the DB
+    public static void main(String[] args) throws Exception {
+
+    }
+}
